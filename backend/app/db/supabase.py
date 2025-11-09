5746b7263b2e5dcf5ea7ee96a4da0f51c3d90cef
Arturiooo <arthur-nahushev@mail.ru>
2025-11-09 23:55:24 +0300

    new req

--- backend/app/db/supabase.py
+++ backend/app/db/supabase.py
@@ -4,10 +4,4 @@
 
 # supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-# # Пример: выбрать все записи из существующей таблицы "scripts"
-# response = supabase.table("scripts").select("*").execute()
-# print(response.data)
 
-# # Пример: вставить новую запись
-# new_record = {"title": "Новый сценарий", "text": "Текст"}
-# supabase.table("scripts").insert(new_record).execute()
